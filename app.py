247deed8c54c88db01c203bd756dcb1d00b39cc1
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-03-17 00:04:57 +0545

    refactor

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-# this will contain the streamlit code with graph invocation
+# Main app
+from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal, TypedDict, Optional, List, Annotated
+from langchain_ollama import OllamaEmbeddings
+from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage, AIMessage, BaseMessage
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+from langgraph.graph.message import add_messages
+from langgraph.types import Command, Interrupt
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+llm = ChatGroq(
+    api_key=os.getenv("api_key"),
+    model="llama-3.3-70b-versatile"
+)
+
+tavily_research_tool = TavilySearch(max_results=3, include_answer= True, search_depth= "advanced", auto_parameters=True)
+tavily_research_tool.invoke({"query" : "What is llm?"})
